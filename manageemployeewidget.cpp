8335993dcaadb63dcee743d5c375173e2274c03a
Sangram Jagtap <sangramjagtap096@gmail.com>
2024-05-06 23:23:37 -0700

    initial setup

--- manageemployeewidget.cpp
+++ manageemployeewidget.cpp
@@ -2,10 +2,32 @@
 #include "ui_manageemployeewidget.h"
 
 ManageEmployeeWidget::ManageEmployeeWidget(QWidget *parent)
-    : QWidget(parent)
-    , ui(new Ui::ManageEmployeeWidget)
+    : QWidget(parent), ui(new Ui::ManageEmployeeWidget)
 {
     ui->setupUi(this);
+
+    // Initially hide the input fields, labels, and Add Employee button
+    ui->lineEdit->setVisible(false);
+    ui->lineEdit_2->setVisible(false);
+    ui->lineEdit_5->setVisible(false);
+    ui->lineEdit_6->setVisible(false);
+    ui->lineEdit_7->setVisible(false);
+    ui->lineEdit_8->setVisible(false);
+    ui->comboBox->setVisible(false);
+    ui->comboBox_2->setVisible(false);
+    ui->pushButton_3->setVisible(false);
+    // Hiding the labels
+    ui->label_2->setVisible(false);
+    ui->label_3->setVisible(false);
+    ui->label_5->setVisible(false);
+    ui->label_6->setVisible(false);
+    ui->label_7->setVisible(false);
+    ui->label_8->setVisible(false);
+    ui->label_9->setVisible(false);
+    ui->label_10->setVisible(false);
+
+    // Connect the Add button's clicked signal to the slot
+    connect(ui->pushButton_2, &QPushButton::clicked, this, &ManageEmployeeWidget::on_addButton_clicked);
 }
 
 ManageEmployeeWidget::~ManageEmployeeWidget()
@@ -18,3 +40,26 @@ void ManageEmployeeWidget::on_pushButton_clicked()
     emit backHome();
 }
 
+void ManageEmployeeWidget::on_addButton_clicked()
+{
+    // Toggle the visibility of the input fields, labels, and Add Employee button
+    bool isVisible = ui->lineEdit->isVisible();
+    ui->lineEdit->setVisible(!isVisible);
+    ui->lineEdit_2->setVisible(!isVisible);
+    ui->lineEdit_5->setVisible(!isVisible);
+    ui->lineEdit_6->setVisible(!isVisible);
+    ui->lineEdit_7->setVisible(!isVisible);
+    ui->lineEdit_8->setVisible(!isVisible);
+    ui->comboBox->setVisible(!isVisible);
+    ui->comboBox_2->setVisible(!isVisible);
+    ui->pushButton_3->setVisible(!isVisible);
+    // Toggling the labels' visibility
+    ui->label_2->setVisible(!isVisible);
+    ui->label_3->setVisible(!isVisible);
+    ui->label_5->setVisible(!isVisible);
+    ui->label_6->setVisible(!isVisible);
+    ui->label_7->setVisible(!isVisible);
+    ui->label_8->setVisible(!isVisible);
+    ui->label_9->setVisible(!isVisible);
+    ui->label_10->setVisible(!isVisible);
+}
--- manageemployeewidget.h
+++ manageemployeewidget.h
@@ -17,6 +17,7 @@ public:
 
 private slots:
     void on_pushButton_clicked();
+    void on_addButton_clicked();
 
 private:
     Ui::ManageEmployeeWidget *ui;
