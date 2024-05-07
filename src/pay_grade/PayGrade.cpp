e02ae4f007c38f0de6dfb134432ea4bca27b009f
purvil-patel <purvil1699@gmail.com>
2024-05-07 13:21:54 -0700

    Created the PaygradeUI

--- src/pay_grade/PayGrade.cpp
+++ src/pay_grade/PayGrade.cpp
@@ -13,7 +13,6 @@ PayGrade::PayGrade(sqlite3* dbConnection) : db(dbConnection) {
         "grade_basic REAL, "
         "grade_da REAL, "
         "grade_ta REAL, "
-        "grade_pf REAL, "
         "grade_bonus REAL);";
 
     char* errMsg = nullptr;
@@ -36,7 +35,7 @@ void PayGrade::handleAddPaygrade() {
 
     std::string department_name;
     std::string grade_name;
-    float grade_basic, grade_da, grade_ta, grade_pf, grade_bonus;
+    float grade_basic, grade_da, grade_ta, grade_bonus;
 
     std::cout << "Select Department Name: ";
     std::getline(std::cin, department_name);
@@ -47,27 +46,40 @@ void PayGrade::handleAddPaygrade() {
     std::cout << "Basic Pay: "; std::cin >> grade_basic; clearInputBuffer();
     std::cout << "Dearness Allowance (DA): "; std::cin >> grade_da; clearInputBuffer();
     std::cout << "Travel Allowance (TA): "; std::cin >> grade_ta; clearInputBuffer();
-    std::cout << "Provident Fund (PF): "; std::cin >> grade_pf; clearInputBuffer();
     std::cout << "Bonus: "; std::cin >> grade_bonus; clearInputBuffer();
 
     // Pass the input data to the database function
-    addPaygradeToDatabase(department_name, grade_name, grade_basic, grade_da, grade_ta, grade_pf, grade_bonus);
+    addPaygradeToDatabase(department_name, grade_name, grade_basic, grade_da, grade_ta, grade_bonus);
 }
 
 void PayGrade::addPaygradeToDatabase(const std::string &department_name, const std::string &grade_name,
-                                     float grade_basic, float grade_da, float grade_ta, float grade_pf, float grade_bonus) {
-    std::string sql = "INSERT INTO PayGrade (department_name, grade_name, grade_basic, grade_da, grade_ta, grade_pf, grade_bonus) VALUES ('"
-                      + department_name + "', '" + grade_name + "', " + std::to_string(grade_basic) + ", "
-                      + std::to_string(grade_da) + ", " + std::to_string(grade_ta) + ", " + std::to_string(grade_pf)
-                      + ", " + std::to_string(grade_bonus) + ");";
-    char *errMsg = nullptr;
-    sqlite3_exec(db, sql.c_str(), nullptr, nullptr, &errMsg);
-    if (errMsg) {
-        std::cerr << "SQL error: " << errMsg << std::endl;
-        sqlite3_free(errMsg);
+                                     float grade_basic, float grade_da, float grade_ta, float grade_bonus) {
+    const char* sql = "INSERT INTO PayGrade (department_name, grade_name, grade_basic, grade_da, grade_ta, grade_bonus) "
+                      "VALUES (?, ?, ?, ?, ?, ?);";
+    sqlite3_stmt* stmt = nullptr;
+
+    if (sqlite3_prepare_v2(db, sql, -1, &stmt, nullptr) != SQLITE_OK) {
+        std::cerr << "SQL error: " << sqlite3_errmsg(db) << std::endl;
+        return;
+    }
+
+    // Bind the parameters securely
+    sqlite3_bind_text(stmt, 1, department_name.c_str(), -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 2, grade_name.c_str(), -1, SQLITE_STATIC);
+    sqlite3_bind_double(stmt, 3, grade_basic);
+    sqlite3_bind_double(stmt, 4, grade_da);
+    sqlite3_bind_double(stmt, 5, grade_ta);
+    sqlite3_bind_double(stmt, 6, grade_bonus);
+
+    // Execute the statement
+    if (sqlite3_step(stmt) != SQLITE_DONE) {
+        std::cerr << "SQL error: " << sqlite3_errmsg(db) << std::endl;
     } else {
         std::cout << "Pay grade successfully added to the database.\n";
     }
+
+    // Clean up
+    sqlite3_finalize(stmt);
 }
 
 std::vector<PayGradeDetail> PayGrade::listPayGradesByDepartment(const std::string& department) {
@@ -75,9 +87,9 @@ std::vector<PayGradeDetail> PayGrade::listPayGradesByDepartment(const std::strin
 
     std::string sql;
     if (department == "all") {
-        sql = "SELECT department_name, grade_name, grade_basic, grade_da, grade_ta, grade_pf, grade_bonus FROM PayGrade ORDER BY department_name;";
+        sql = "SELECT department_name, grade_name, grade_basic, grade_da, grade_ta, grade_bonus FROM PayGrade ORDER BY department_name;";
     } else {
-        sql = "SELECT grade_name, grade_basic, grade_da, grade_ta, grade_pf, grade_bonus FROM PayGrade WHERE department_name = '" + department + "';";
+        sql = "SELECT grade_name, grade_basic, grade_da, grade_ta, grade_bonus FROM PayGrade WHERE department_name = '" + department + "';";
     }
 
     char* zErrMsg = 0;
@@ -95,16 +107,14 @@ std::vector<PayGradeDetail> PayGrade::listPayGradesByDepartment(const std::strin
             detail.basic_pay = sqlite3_column_double(stmt, 2);
             detail.dearness_allowance = sqlite3_column_double(stmt, 3);
             detail.travel_allowance = sqlite3_column_double(stmt, 4);
-            detail.provident_fund = sqlite3_column_double(stmt, 5);
-            detail.bonus = sqlite3_column_double(stmt, 6);
+            detail.bonus = sqlite3_column_double(stmt, 5);
         } else {
             detail.department_name = department; // Use the provided department name directly
             detail.grade_name = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0));
             detail.basic_pay = sqlite3_column_double(stmt, 1);
             detail.dearness_allowance = sqlite3_column_double(stmt, 2);
             detail.travel_allowance = sqlite3_column_double(stmt, 3);
-            detail.provident_fund = sqlite3_column_double(stmt, 4);
-            detail.bonus = sqlite3_column_double(stmt, 5);
+            detail.bonus = sqlite3_column_double(stmt, 4);
         }
         payGrades.push_back(detail);
     }
@@ -121,13 +131,12 @@ void PayGrade::printPayGrades(const std::vector<PayGradeDetail>& payGrades) {
     
     std::cout << "Paygrades Details:" << std::endl;
     std::cout << std::left << std::setw(20) << "Department" << std::setw(15) << "Grade Name" << std::setw(10) << "Basic"
-              << std::setw(10) << "DA" << std::setw(10) << "TA" << std::setw(10) << "PF" << std::setw(10) << "Bonus" << std::endl;
+              << std::setw(10) << "DA" << std::setw(10) << "TA" << std::setw(10) << "Bonus" << std::endl;
     
     for (const auto& detail : payGrades) {
         std::cout << std::left << std::setw(20) << detail.department_name << std::setw(15) << detail.grade_name 
                   << std::setw(10) << detail.basic_pay << std::setw(10) << detail.dearness_allowance 
-                  << std::setw(10) << detail.travel_allowance << std::setw(10) << detail.provident_fund 
-                  << std::setw(10) << detail.bonus << std::endl;
+                  << std::setw(10) << detail.travel_allowance << std::setw(10) << detail.bonus << std::endl;
     }
 }
 
@@ -146,18 +155,17 @@ void PayGrade::handleDepartmentSelectionAndDisplay() {
     }
 }
 
-void PayGrade::collectUpdateInputs(std::string& gradeToUpdate, float& basic, float& da, float& ta, float& pf, float& bonus) {
+void PayGrade::collectUpdateInputs(std::string& gradeToUpdate, float& basic, float& da, float& ta, float& bonus) {
     std::cout << "Enter the name of the pay grade to update: ";
     std::getline(std::cin >> std::ws, gradeToUpdate);
 
     std::cout << "Enter new Basic Pay: "; std::cin >> basic; clearInputBuffer();
     std::cout << "Enter new Dearness Allowance (DA): "; std::cin >> da; clearInputBuffer();
     std::cout << "Enter new Travel Allowance (TA): "; std::cin >> ta; clearInputBuffer();
-    std::cout << "Enter new Provident Fund (PF): "; std::cin >> pf; clearInputBuffer();
     std::cout << "Enter new Bonus: "; std::cin >> bonus; clearInputBuffer();
 }
 
-void PayGrade::updatePayGradeDetails(const std::string& gradeToUpdate, float basic, float da, float ta, float pf, float bonus) {
+void PayGrade::updatePayGradeDetails(const std::string& gradeToUpdate, float basic, float da, float ta, float bonus) {
     // Verify if the pay grade exists
     std::string checkSql = "SELECT grade_name FROM PayGrade WHERE grade_name = ?";
     sqlite3_stmt* checkStmt;
@@ -175,7 +183,7 @@ void PayGrade::updatePayGradeDetails(const std::string& gradeToUpdate, float bas
     sqlite3_finalize(checkStmt);
 
     // Prepare the update statement
-    std::string sql = "UPDATE PayGrade SET grade_basic = ?, grade_da = ?, grade_ta = ?, grade_pf = ?, grade_bonus = ? WHERE grade_name = ?";
+    std::string sql = "UPDATE PayGrade SET grade_basic = ?, grade_da = ?, grade_ta = ?, grade_bonus = ? WHERE grade_name = ?";
     sqlite3_stmt* stmt;
     if (sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, nullptr) != SQLITE_OK) {
         std::cerr << "SQL error: " << sqlite3_errmsg(db) << std::endl;
@@ -186,9 +194,8 @@ void PayGrade::updatePayGradeDetails(const std::string& gradeToUpdate, float bas
     sqlite3_bind_double(stmt, 1, basic);
     sqlite3_bind_double(stmt, 2, da);
     sqlite3_bind_double(stmt, 3, ta);
-    sqlite3_bind_double(stmt, 4, pf);
-    sqlite3_bind_double(stmt, 5, bonus);
-    sqlite3_bind_text(stmt, 6, gradeToUpdate.c_str(), -1, SQLITE_STATIC);
+    sqlite3_bind_double(stmt, 4, bonus);
+    sqlite3_bind_text(stmt, 5, gradeToUpdate.c_str(), -1, SQLITE_STATIC);
 
     // Execute the update statement
     if (sqlite3_step(stmt) != SQLITE_DONE) {
@@ -227,7 +234,6 @@ PayGradeDetail PayGrade::getPayGradeDetailsByName(const std::string& grade_name)
         detail.basic_pay = sqlite3_column_double(stmt, 1);
         detail.dearness_allowance = sqlite3_column_double(stmt, 2);
         detail.travel_allowance = sqlite3_column_double(stmt, 3);
-        detail.provident_fund = sqlite3_column_double(stmt, 4);
         detail.bonus = sqlite3_column_double(stmt, 5);
     } else {
         std::cerr << "Pay grade '" << grade_name << "' not found.\n";
--- src/pay_grade/PayGrade.h
+++ src/pay_grade/PayGrade.h
@@ -6,36 +6,30 @@
 #include <vector>
 #include <iostream>
 
-// Define a structure to store pay grade details
+// Define a structure to store paygrade details
 struct PayGradeDetail {
     std::string department_name;
     std::string grade_name;
     float basic_pay;
     float dearness_allowance;
     float travel_allowance;
-    float provident_fund;
     float bonus;
 };
 
 class PayGrade {
 private:
-    sqlite3* db;  // Database connection
+    sqlite3* db;
 
 public:
-    // Constructor that initializes with a database connection
-    explicit PayGrade(sqlite3* dbConnection);
-
-    // Methods to handle pay grade CRUD operations
+    PayGrade(sqlite3* dbConnection);
     void handleAddPaygrade();
     void addPaygradeToDatabase(const std::string& department_name, const std::string& grade_name,
-                               float grade_basic, float grade_da, float grade_ta, float grade_pf, float grade_bonus);
+                           float grade_basic, float grade_da, float grade_ta, float grade_bonus);
     std::vector<PayGradeDetail> listPayGradesByDepartment(const std::string& department);
     void printPayGrades(const std::vector<PayGradeDetail>& payGrades);
     void handleDepartmentSelectionAndDisplay();
-    void collectUpdateInputs(std::string& gradeToUpdate, float& basic, float& da, float& ta, float& pf, float& bonus);
-    void updatePayGradeDetails(const std::string& gradeToUpdate, float basic, float da, float ta, float pf, float bonus);
-
-    // Fetch details of a pay grade by name
+    void collectUpdateInputs(std::string& gradeToUpdate, float& basic, float& da, float& ta, float& bonus);
+    void updatePayGradeDetails(const std::string& gradeToUpdate, float basic, float da, float ta, float bonus);
     PayGradeDetail getPayGradeDetailsByName(const std::string& grade_name);
 
     // Function to collect grade name from user input
